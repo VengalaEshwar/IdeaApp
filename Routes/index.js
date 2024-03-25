6453cfe0a9c520de222e583e0f153487c0135fc4
Eshwar <eshwarvengala30@gmail.com>
2024-03-25 13:18:13 +0530

    Created server and Model

--- Routes/index.js
+++ Routes/index.js
@@ -1,1 +1,11 @@
+//importing the express.js
 const express = require('express');
+
+const app = express();
+
+//start the server
+app.listen(4455,
+    () => {
+        console.log("Server started at port 4455");
+    });
+     
